c731cc2e04c3c98aa50561000c8a3d912f4638ce
eliasevangelho <20251pin0020027@estudantes.ifpr.edu.br>
2025-11-10 11:12:33 -0300

    atvdd 1

--- lista_1_funcoes/atv1.cpp
+++ lista_1_funcoes/atv1.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 using namespace std;
 
-// EXERCÍCIO: CRIE UMA FUNÇÃO NOMEADA apresentar_curso QUE RECEBE O NOME DE UM CURSO 
-// (EXEMPLO: "Informática", "Eletrotécnica") E IMPRIME UMA MENSAGEM DE APRESENTAÇÃO. 
+string apresentar_curso(string curso){
+
+    cout << "Bem-vindo ao curso de " << curso << endl;
+
+}
+
+
+int main(){
+    cout << apresentar_curso("Informática");
+    cout << apresentar_curso("Eletrotécnica");
+    cout << apresentar_curso("Administração");
+}
+
+
+// EXERCÍCIO: CRIE UMA FUNÇÃO NOMEADA apresentar_curso QUE RECEBE O NOME DE UM CURSO
+// (EXEMPLO: "Informática", "Eletrotécnica") E IMPRIME UMA MENSAGEM DE APRESENTAÇÃO.
 // NO main, CHAME A FUNÇÃO PARA 3 CURSOS DIFERENTES.
 
 // Exemplo de saída:
 // Bem-vindo ao curso de Informática
-
-int main(){
-    apresentar_curso("Informática");
-    apresentar_curso("Eletrotécnica");
-    apresentar_curso("Administração");
-}
-
 
 
