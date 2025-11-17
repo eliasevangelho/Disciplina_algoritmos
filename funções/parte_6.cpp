6350ccb401d84e04c2c7aed9f9fb5d028d9fb7bf
eliasevangelho <20251pin0020027@estudantes.ifpr.edu.br>
2025-11-17 14:11:33 -0300

    parte 6

---
+++ funções/parte_6.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int le_vetor(string msg){
+    int num;
+    cin >> num;
+    return num;
+}
+
+void dobra_vetor(int vetor[], int num){
+    for(int i = 0; i < num; i++){
+        vetor[i] = vetor[i] * 2;
+
+    }
+
+
+}
+
+int main(){
+    int vet_numeros[5], tam = 5;
+
+    cout << "Digite 5 numeros para serem dobrados: " << endl;
+
+    for(int i = 0; i < tam; i++){
+        vet_numeros[i] = le_vetor("oioioi");
+    }
+
+
+    cout << "O dobro dos valores digitados: " << endl;
+
+    dobra_vetor(vet_numeros, tam);
+
+
+    for(int i = 0; i < tam; i++){
+        cout << vet_numeros[i] << " ";
+    }
+    cout << endl;
+
+
+
+
+
+
+
+
+
+}
