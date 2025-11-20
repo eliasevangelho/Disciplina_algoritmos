cf158cddad292b11354eabbe42650a9decad6e32
eliasevangelho <20251pin0020027@estudaantes.ifpr.edu.br>
2025-11-19 21:45:03 -0300

    Agora o programa triplica os números (Feito no terminal do linux)

--- funções/parte_6.cpp
+++ funções/parte_6.cpp
@@ -9,7 +9,7 @@ int le_vetor(string msg){
 
 void dobra_vetor(int vetor[], int num){
     for(int i = 0; i < num; i++){
-        vetor[i] = vetor[i] * 2;
+        vetor[i] = vetor[i] * 3;
 
     }
 }
@@ -17,14 +17,14 @@ void dobra_vetor(int vetor[], int num){
 int main(){
     int vet_numeros[5], tam = 5;
 
-    cout << "Digite 5 numeros para serem dobrados: " << endl;
+    cout << "Digite 5 numeros para serem triplicados: " << endl;
 
     for(int i = 0; i < tam; i++){
         vet_numeros[i] = le_vetor("oioioi");
     }
 
 
-    cout << "O dobro dos valores digitados: " << endl;
+    cout << "O triplo dos valores digitados: " << endl;
 
     dobra_vetor(vet_numeros, tam);
 
